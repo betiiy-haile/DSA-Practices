55a08bdc0c4263801050b579eee1203772592d97
Betelhem Haile <108091111+betiiy-haile@users.noreply.github.com>
2025-08-05 10:13:41 +0300

    Time: 7 ms (23.39%), Space: 6 MB (78.33%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.go
@@ -0,0 +1,14 @@
+func isPalindrome(x int) bool {
+    if x < 0 {
+        return false
+    }
+    original := x
+    reversed := 0
+
+    for x > 0 {
+        reversed = reversed*10 + x%10
+        x /= 10
+    }
+
+    return original == reversed
+}
