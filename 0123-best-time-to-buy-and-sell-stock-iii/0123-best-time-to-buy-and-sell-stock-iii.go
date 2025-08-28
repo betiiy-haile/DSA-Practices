d70fb3aaae2f0145143185f027e4be5fc5e2f710
Betelhem Haile <108091111+betiiy-haile@users.noreply.github.com>
2025-08-28 18:57:46 +0300

    Time: 0 ms (100%), Space: 10.8 MB (55.38%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.go
@@ -0,0 +1,12 @@
+func maxProfit(prices []int) int {
+    buy1, sell1 := math.MinInt32, 0
+    buy2, sell2 := math.MinInt32, 0
+    
+    for _, p := range prices {
+        buy1 = max(buy1, -p)
+        sell1 = max(sell1, buy1 + p)
+        buy2 = max(buy2, sell1 - p)
+        sell2 = max(sell2, buy2 + p)
+    }
+    return sell2
+}
