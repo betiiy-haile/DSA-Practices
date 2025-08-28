183bc3a76a2dcee059f00e672fe863e350d9f71d
Betelhem Haile <108091111+betiiy-haile@users.noreply.github.com>
2025-08-28 18:44:39 +0300

    Time: 0 ms (100%), Space: 5.2 MB (15.39%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.go
@@ -0,0 +1,10 @@
+func maxProfit(prices []int) int {
+    profit := 0
+
+    for i := 1; i < len(prices); i++ {
+        if prices[i - 1] <= prices[i]{
+            profit += prices[i] - prices[i - 1]
+        }
+    }
+    return profit
+}
